e2f74c671ef85e30e6b112c61c324b30add6e98e
gildeir <gildeirlopes@gmail.com>
2025-02-06 02:33:45 -0300

    feat: adding pacienteController and Repository

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,42 @@
+﻿using MedicalOfficeApi.Model.Entities;
+using MedicalOfficeApi.Repository;
+using MedicalOfficeApi.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalOfficeApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PacienteController: ControllerBase
+    {
+        private readonly IPacienteRepository _pacienteRepository;
+
+        public PacienteController(IPacienteRepository pacientePacienteRepository)
+        {
+            _pacienteRepository = pacientePacienteRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Paciente>>> Get()
+        {
+            var result = await _pacienteRepository.Get();
+
+            return result.ToList();
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Paciente>> GetById(int id)
+        {
+            var paciente = await _pacienteRepository.GetById(id);
+           
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            return Ok(paciente);
+        }
+
+
+    }
+}
---
+++ Repository/PacienteRepository.cs
@@ -0,0 +1,58 @@
+﻿using MedicalOfficeApi.Context;
+using MedicalOfficeApi.Model.Entities;
+using MedicalOfficeApi.Repository.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalOfficeApi.Repository
+{
+    public class PacienteRepository : BaseRepository, IPacienteRepository
+    {
+        private readonly MediaOfficeContext _context;
+
+        public PacienteRepository(MediaOfficeContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Paciente>> Get()
+        {
+            return await _context.Pacientes.ToListAsync();
+        }
+
+        public async Task<Paciente> GetById(int id)
+        {
+            return await _context.Pacientes.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var remove = await _context.Pacientes.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (remove is not null)
+            {
+                _context.Pacientes.Remove(remove);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<Paciente> Create(Paciente paciente)
+        {
+            _context.Pacientes.Add(paciente);
+            await _context.SaveChangesAsync();
+            return paciente;
+        }
+
+        public async Task<Paciente> Update(Paciente paciente)
+        {
+            _context.Pacientes.Update(paciente);
+
+            await _context.SaveChangesAsync();
+
+            return paciente;
+        }
+    }
+}
